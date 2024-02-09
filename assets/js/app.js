7f77a7aadafb37a63f752161d8dddc3fa0e97bf5
Mehmet Genç <gencaslan555@gmail.com>
2024-02-09 15:28:30 +0300

    mehmet

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,15 +7,46 @@ const fullName = document.querySelector(".input-text");
 let participantList = {
   "1":"Sinan",
   "2":"Eda",
-  "3":"Sule"
+  "3":"Sule",
+  "4":"Tuba",
+  "5":"Mehmet",
+  "6":"Cahit",
+  "7":"Emrullah",
+  "8":"Asiye",
+
 }
 
+//----------------MEHMET-----------------------------
+const randomCreate = (obj,type) => { 
+
+  if(type===1){
+    const keys=Object.keys(obj);
+    const randomIndex=Math.floor(Math.random()*keys.length);
+    return keys[randomIndex];
+  }else if(type===2){
+    const keys=Object.keys(obj);
+    for(let i=keys.length-1;i>0;i--){
+      const j=Math.floor(Math.random()*(i+1));
+      [keys[i],keys[j]]=[keys[j],keys[i]];
+      return keys;
+  }
+  }else{
+    alert("type 1 veya 2 olmalıdır")
+  }
+ }
+console.log(randomCreate(participantList,1))
+console.log(randomCreate(participantList,2))
+
 // Mehmet
-function getRandomNumber(){
- //Listedeki sayilarin random numarasini getir
+const getRandomNumber = (obj,type) => { 
+randomCreate(obj,type);
+
+//Listedeki sayilarin random numarasini getir
 //  return kisinin keyi
 }
 // Mehmet
+//----------------MEHMET-----------------------------
+
 
 // Emrullah
 function cekilis() {

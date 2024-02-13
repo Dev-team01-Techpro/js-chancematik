c7976f904958b4fcc4ec00dc5d4e8d54e5fd93d5
Eda Sarıkaya <edassarkaya@outlook.com>
2024-02-13 20:34:33 +0000

    updated

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,120 +1,24 @@
-const winner = document.querySelector(".winner");
-const addParticipant = document.querySelector(".add-participant");
-const box = document.querySelector(".box");
-const winnerName = document.querySelector(".winnerName");
-const fullName = document.querySelector(".input-text");
-const raffleInp = document.querySelector(".raffle");
 
-let participantList = {
-  1: "Sinan",
-  2: "Eda",
-  3: "Sule",
-  4: "Tuba",
-  5: "Mehmet",
-  6: "Cahit",
-  7: "Emrullah",
-  8: "Asiye",
-};
 
-//----------------MEHMET-----------------------------
-const getRandomNumber = (obj, type) => {
-  if (type === 1) {
-    const keys = Object.keys(obj);
-    const randomIndex = Math.floor(Math.random() * keys.length);
-    return keys[randomIndex];
-  } else if (type === 2) {
-    const keys = Object.keys(obj);
-
-    keys.sort(() => Math.random() - 0.5);
-
-    return keys;
-  }
-};
-
-console.log(getRandomNumber(participantList, 1));
-console.log(getRandomNumber(participantList, 2));
-//----------------MEHMET-----------------------------
-
-//----------------Emrullah---------------------------
-// raffleInp.addEventListener
-const raffle = () => {
-  winner.classList.add("active");
-  // kisiler.kisininKeyi
-  // winnerName.innerText = "Sinan";
-
-  let kisininKeyi = getRandomNumber(participantList, 1);
-
-  winnerName.innerText = kisininKeyi;
-  //console.log(name);
-  setTimeout(() => {
-    winner.classList.remove("active");
-  }, 5000);
-};
-// raffle();
-//----------------Emrullah---------------------------
-
-// Tuba
-// fullName value degerini participantList'e ekle
-function addPart() {
-  addParticipant.classList.add("active");
-  box.classList.add("active");
-}
-
-function exitBox() {
-  addParticipant.classList.remove("active");
-  box.classList.remove("active");
-}
-
-// Cahit
-// Hangi gunler checked oldugunu kontrol et ve arraya at
-// fonksiyon icereisne yap. Return fonksiyon
-// DayCheck()
-// const cars = ["Saab", "Volvo", "BMW"];
-// return cars;
-
-// Eda
-// Gunlere dagit foknsiyonu
-
-const randomNumber = (max) => {
-  let i = 0;
-  let arr = [];
-
-  while (i + 1 <= max) {
-    let randomN = Math.floor(Math.random() * max) + 1;
-
-    if (randomN != 0 && !arr.includes(randomN)) {
-      arr.push(randomN);
-
-      i++;
-    }
-  }
- return arr;
-};
+//----------------Eda---------------------------------
 
 
 const matchUp = () => {
-  
-let days=["pazartesi", "sali","çarşamba","persembe","cuma"];
+  let days = ["pazartesi", "sali", "çarşamba", "persembe", "cuma"];
+  let participantCount = Object.keys(participantList).length;
+  let arrMatch = randomNumber(participantCount);
 
-  let arrMatch = randomNumber(9);
+  for (let i = 0; i < participantCount; i++) {
 
-  for(let i=0; i<days.length; i++){
-    days[i]=arrMatch[i];
+    
+    let participantIndex = arrMatch[i] - 1;
+    let participant = participantList[participantIndex + 1];
+    let dayIndex = i % days.length;
 
-    console.log(days[i])
+
+    console.log(`${days[dayIndex]}: ${participant}`);
   }
-console.log(days)
-
-
 };
-
 matchUp()
 
-// DayCheck()
-
-const participantBody = document.querySelector(".participant-body");
-
-participantBody.addEventListener("click", (e) => {
-  console.log(e.target.id);
-});
-
+//----------------Eda----------------------------------
